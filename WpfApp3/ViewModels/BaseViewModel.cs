9688eefe189a2f47989a9123619b18cebd49b1e2
40338732 <40338732@users.noreply.github.com>
2020-05-30 15:35:34 +0100

    Update point 26

--- WpfApp3/ViewModels/BaseViewModel.cs
+++ WpfApp3/ViewModels/BaseViewModel.cs
@@ -7,6 +7,7 @@ using System.Threading.Tasks;
 
 namespace WpfApp3.ViewModels
 {
+    // change 
     public class BaseViewModel : INotifyPropertyChanged
     {
         public event PropertyChangedEventHandler PropertyChanged;
