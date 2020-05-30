fd145ea955297b4abb4bd0fabb87f4ed7e580049
40338732 <40338732@users.noreply.github.com>
2020-05-30 14:20:17 +0100

    Build to first Run (point 9)

---
+++ WpfApp3/ViewModels/MainWindowViewModel .cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WpfApp3.ViewModels
+{
+    class MainWindowViewModel : BaseViewModel
+    {
+        public string NameTextBlock { get; private set; }
+        public string AgeTextBlock { get; private set; }
+        public string AddressTextBlock { get; private set; }
+
+
+        public string ClearButtonText { get; private set; }
+        public string SaveButtonText { get; private set; }
+
+        public MainWindowViewModel()
+        {
+            NameTextBlock = "Name";
+            AgeTextBlock = "Age";
+            AddressTextBlock = "Address";
+
+            ClearButtonText = "Clear";
+            SaveButtonText = "Save";
+        }
+    }
+
+    
+    
+}
